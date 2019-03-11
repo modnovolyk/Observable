77d63589019115c223b3df091c141c29b4ad3034
Max Odnovolyk <modnovolyk@gmail.com>
2019-03-11 15:21:28 +0200

    Add xcode project file with framework target

---
+++ Observable/Observable.h
@@ -0,0 +1,19 @@
+//
+//  Observable.h
+//  Observable
+//
+//  Created by Max Odnovolyk on 3/11/19.
+//  Copyright © 2019 modnovolyk. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Observable.
+FOUNDATION_EXPORT double ObservableVersionNumber;
+
+//! Project version string for Observable.
+FOUNDATION_EXPORT const unsigned char ObservableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Observable/PublicHeader.h>
+
+
